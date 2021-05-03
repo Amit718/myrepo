d2ea95d3904feba0890237b7e63152fd56809a14
Amit J <ja.amit718@gmail.com>
2021-05-03 16:06:59 +0530

    Home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelll
+                    Laravell
                 </div>
 
                 <div class="links">
